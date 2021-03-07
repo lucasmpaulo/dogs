002e022766a1704da94c2e8e0bcdf036ef71d9c8
Lucas Paulo <mesakilive@gmail.com>
2021-03-07 10:31:45 -0300

    Adicionado  a API com a URL que irá puxar o token de autenticação

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from './Input.module.css'; 
 
-const Input = ({label, type, name}) => {
+const Input = ({label, type, name, value, onChange, error, onBlur}) => {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label} htmlFor={name}>{label}</label>
-      <input type="text" className={styles.input} type={type} name={name} />
-      <p className={styles.error}>Error</p>
+      <input type="text" 
+        id={name} 
+        className={styles.input} 
+        type={type} name={name}
+        onChange={onChange} 
+        value={value} 
+        onBlur={onBlur} 
+      />
+      
+      { error && <p className={styles.error}>{error}</p>}
     </div>
   );
   
