ec6ab6ea859f732776c4db09a61dbe4169dea7e7
Lucas Paulo <mesakilive@gmail.com>
2021-03-17 18:16:00 -0300

    Adicionado delete nos posts

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -4,7 +4,6 @@ const useFetch = () => {
   const [data, setData] = React.useState(null);
   const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
-
   const request = React.useCallback(async (url, options) => {
     let response;
     let json;
