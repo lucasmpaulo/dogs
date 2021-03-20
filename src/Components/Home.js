5d279b7670e992ef56999c206d5f953c44d5547a
Lucas Paulo <mesakilive@gmail.com>
2021-03-20 09:41:12 -0300

    Adicionado header e footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Feed from './Feed/Feed'
+import Head from './Helper/Head'
 import Loading from './Helper/Loading'
 
 const Home = () => {
   return (
     <section className="container mainContainer">
-      {/* <Loading /> */}
+      <Head title="Fotos" description="Home do site dogs, com o feed de fotos." />
       <Feed />
     </section>
   )
--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -6,6 +6,7 @@ import useFetch from '../../Hooks/useFetch';
 import { PASSWORD_RESET } from '../../Api';
 import Error from '../Helper/Error';
 import { useNavigate } from 'react-router-dom';
+import Head from '../Helper/Head';
 
 const LoginPasswordReset = () => {
   const [login, setLogin] = React.useState('');
@@ -37,6 +38,7 @@ const LoginPasswordReset = () => {
 
   return (
     <div>
+      <Head title="Redefina a senha" />
       <h1 className="title">Resete a senha</h1>
       <form onSubmit={handleSubmit}>
         <Input label="Nova senha" type="password" name="password" {...password} />
